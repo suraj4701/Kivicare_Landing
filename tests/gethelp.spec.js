b11f16e6fd4fda0923739b5827fdcad7f256c0f1
Suraj Dholakiya <suraj.goldenmace@gmail.com>
2026-02-17 12:46:18 +0530

    Refactor GetHelp tests to utilize CommonLinkVerify for improved link validation

--- tests/gethelp.spec.js
+++ tests/gethelp.spec.js
@@ -1,30 +1,21 @@
 import { expect, test } from '@playwright/test';
+import { CommonLinkVerify } from './common';
 const home_url = process.env.HOME_URL;
 
 test("GetHelp Support", async ({ page }) => {
     await page.goto(home_url);
     await page.locator("//li[@id='menu-item-832']").hover()
     const support = page.locator("//li[@id='menu-item-833']")
-
-    const [newPage] = await Promise.all([
-        page.context().waitForEvent('page'),
-        support.click()
-    ])
-    const newPageUrl = newPage.url();
-    expect(newPageUrl).toBe("https://iqonic.desky.support/");
+    const expectedLink = "https://iqonic.desky.support/";
+    await CommonLinkVerify(page, support, expectedLink);
 })
 
 test("GetHelp Docs", async ({ page }) => {
     await page.goto(home_url);
     await page.locator("//li[@id='menu-item-832']").hover()
     const support = page.locator("//li[@id='menu-item-834']")
-
-    const [newPage] = await Promise.all([
-        page.context().waitForEvent('page'),
-        support.click()
-    ])
-    const newPageUrl = newPage.url();
-    expect(newPageUrl).toBe("https://documentation.iqonic.design/kivicare-laravel/");
+    const expectedLink = "https://documentation.iqonic.design/kivicare-laravel/";
+    await CommonLinkVerify(page, support, expectedLink);
 })
 
 test("GetHelp FAQ", async ({ page }) => {
