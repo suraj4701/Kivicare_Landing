a1f6cd3d953d3572eb18794604eae74081a28ff9
Suraj Dholakiya <suraj.goldenmace@gmail.com>
2026-03-12 17:31:38 +0530

    feat: Add Playwright tests for AdminPanel and ClinicAdminApp.

--- tests/adminpanel.spec.js
+++ tests/adminpanel.spec.js
@@ -10,82 +10,82 @@ test.describe("AdminPanel", () => {
     });
 
     test("Envanto Kivicare Verify", async ({ page }) => {
-        const EnvantoKivicareVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a[1]");
-        await EnvantoKivicareVerify(page, EnvantoKivicareVerifyLocator);
+        const adminPanelEnvantoKivicareLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a[1]");
+        await EnvantoKivicareVerify(page, adminPanelEnvantoKivicareLocator);
     });
 
     test("Admin Panel Verify", async ({ page }) => {
-        const adminpanelLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a[1]");
-        await AdminPanelVerify(page, adminpanelLocator);
+        const adminPanelVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a[1]");
+        await AdminPanelVerify(page, adminPanelVerifyLocator);
     });
 
     test("Trustpilot Verify", async ({ page }) => {
-        const TrustpilotVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/a[1]");
-        await TrustpilotVerify(page, TrustpilotVerifyLocator);
+        const adminPanelTrustpilotVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/a[1]");
+        await TrustpilotVerify(page, adminPanelTrustpilotVerifyLocator);
     });
 
     test("Envanto Verify", async ({ page }) => {
-        const EnvantoVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/a[1]");
-        await EnvantoVerify(page, EnvantoVerifyLocator);
+        const adminPanelEnvantoVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/a[1]");
+        await EnvantoVerify(page, adminPanelEnvantoVerifyLocator);
     });
 
     test("Book a quick call", async ({ page }) => {
-        const EnvantoVerifyLocator = page.locator("//a[contains(text(),'Book a quick call.')]");
-        await BookcallVerify(page, EnvantoVerifyLocator);
+        const adminPanelBookCallLocator = page.locator("//a[contains(text(),'Book a quick call.')]");
+        await BookcallVerify(page, adminPanelBookCallLocator);
     });
 
     test("Book a quick call 2", async ({ page }) => {
-        const EnvantoVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]");
-        await EnvantoVerifyLocator.scrollIntoViewIfNeeded();
-        await BookcallVerify(page, EnvantoVerifyLocator);
+        const adminPanelBookCallLocator2 = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[3]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]");
+        await adminPanelBookCallLocator2.scrollIntoViewIfNeeded();
+        await BookcallVerify(page, adminPanelBookCallLocator2);
     });
 
     test("View Demo", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/bed-allocation')]");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/bed-allocation')]");
+        await adminPanelViewDemoLocator.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator);
     });
 
     test("View Demo 2", async ({ page }) => {
-        const adminpanelLocator = page.locator("//div[contains(@class,'elementor-element elementor-element-07e4802 elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator2 = page.locator("//div[contains(@class,'elementor-element elementor-element-07e4802 elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
+        await adminPanelViewDemoLocator2.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator2);
     });
 
     test("View Demo 3", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/billing-record')]");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator3 = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/billing-record')]");
+        await adminPanelViewDemoLocator3.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator3);
     });
 
     test("View Demo 4", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/customers')]");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator4 = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/customers')]");
+        await adminPanelViewDemoLocator4.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator4);
     });
 
     test("View Demo 5", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/encounter')]//*[name()='svg']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator5 = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/encounter')]//*[name()='svg']");
+        await adminPanelViewDemoLocator5.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator5);
     });
 
     test("for doctors", async ({ page }) => {
-        const LinkLocator = page.locator("//a[normalize-space()='for doctors']");
-        await LinkLocator.scrollIntoViewIfNeeded();
+        const adminPanelDoctorsLinkLocator = page.locator("//a[normalize-space()='for doctors']");
+        await adminPanelDoctorsLinkLocator.scrollIntoViewIfNeeded();
         const expectedLink = "https://laravel.kivicare.io/laravel-clinic-management-system-for-medical-professionals/";
-        await CommonLinkVerify(page, LinkLocator, expectedLink);
+        await CommonLinkVerify(page, adminPanelDoctorsLinkLocator, expectedLink);
     });
 
     test("View Demo 6", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/earnings')]");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator6 = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/earnings')]");
+        await adminPanelViewDemoLocator6.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator6);
     });
 
     test("View Demo 7", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/backups')]");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await PatientWebsiteLoginVerify(page, adminpanelLocator);
+        const adminPanelViewDemoLocator7 = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/backups')]");
+        await adminPanelViewDemoLocator7.scrollIntoViewIfNeeded();
+        await PatientWebsiteLoginVerify(page, adminPanelViewDemoLocator7);
     });
 });
--- tests/clinicadminapp.spec.js
+++ tests/clinicadminapp.spec.js
@@ -10,70 +10,70 @@ test.describe("ClinicAdminApp", () => {
     });
 
     test("Envanto Kivicare Verify", async ({ page }) => {
-        const EnvantoKivicareVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a[1]");
-        await EnvantoKivicareVerify(page, EnvantoKivicareVerifyLocator);
+        const clinicAdminEnvantoKivicareLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[1]/div[1]/div[1]/div[1]/a[1]");
+        await EnvantoKivicareVerify(page, clinicAdminEnvantoKivicareLocator);
     });
 
     test("Employee App Playstore", async ({ page }) => {
-        const adminpanelLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a[1]");
-        await EmployeeAppPlaystore(page, adminpanelLocator);
+        const clinicAdminPlaystoreLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[2]/div[2]/div[1]/div[1]/div[1]/a[1]");
+        await EmployeeAppPlaystore(page, clinicAdminPlaystoreLocator);
     });
 
     test("Trustpilot Verify", async ({ page }) => {
-        const TrustpilotVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/a[1]");
-        await TrustpilotVerify(page, TrustpilotVerifyLocator);
+        const clinicAdminTrustpilotVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[1]/div[1]/a[1]");
+        await TrustpilotVerify(page, clinicAdminTrustpilotVerifyLocator);
     });
 
     test("Envanto Verify", async ({ page }) => {
-        const EnvantoVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/a[1]");
-        await EnvantoVerify(page, EnvantoVerifyLocator);
+        const clinicAdminEnvantoVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[1]/div[1]/div[1]/div[3]/div[3]/div[1]/a[1]");
+        await EnvantoVerify(page, clinicAdminEnvantoVerifyLocator);
     });
 
     test("Book a quick call", async ({ page }) => {
-        const EnvantoVerifyLocator = page.locator("//a[contains(text(),'Book a quick call.')]");
-        await BookcallVerify(page, EnvantoVerifyLocator);
+        const clinicAdminBookCallLocator = page.locator("//a[contains(text(),'Book a quick call.')]");
+        await BookcallVerify(page, clinicAdminBookCallLocator);
     });
 
     test("Book a quick call 2", async ({ page }) => {
-        const EnvantoVerifyLocator = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]");
-        await EnvantoVerifyLocator.scrollIntoViewIfNeeded();
-        await BookcallVerify(page, EnvantoVerifyLocator);
+        const clinicAdminBookCallLocator2 = page.locator("//body/div[@id='main-container']/main[@id='main']/div[1]/article[1]/div[1]/div[1]/div[5]/div[1]/div[1]/div[1]/div[1]/div[1]/div[1]/a[1]");
+        await clinicAdminBookCallLocator2.scrollIntoViewIfNeeded();
+        await BookcallVerify(page, clinicAdminBookCallLocator2);
     });
 
     test("View Demo", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/bed-allocation')]");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
+        const clinicAdminViewDemoLocator = page.locator("//a[contains(@href,'https://apps.iqonic.design/kivicare-laravel/app/bed-allocation')]");
+        await clinicAdminViewDemoLocator.scrollIntoViewIfNeeded();
         const expectedLink = "https://apps.iqonic.design/kivicare-laravel/login";
-        await CommonLinkVerify(page, adminpanelLocator, expectedLink);
+        await CommonLinkVerify(page, clinicAdminViewDemoLocator, expectedLink);
     });
 
     test("View Demo 2", async ({ page }) => {
-        const adminpanelLocator = page.locator("//div[contains(@class,'elementor-element elementor-element-22c899a elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await EmployeeAppPlaystore(page, adminpanelLocator);
+        const clinicAdminViewDemoLocator2 = page.locator("//div[contains(@class,'elementor-element elementor-element-22c899a elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
+        await clinicAdminViewDemoLocator2.scrollIntoViewIfNeeded();
+        await EmployeeAppPlaystore(page, clinicAdminViewDemoLocator2);
     });
 
     test("Handle all clinic management needs", async ({ page }) => {
-        const adminpanelLocator = page.locator("//a[normalize-space()='Handle all clinic management needs']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await EmployeeAppAppstore(page, adminpanelLocator);
+        const clinicAdminHandleNeedsLocator = page.locator("//a[normalize-space()='Handle all clinic management needs']");
+        await clinicAdminHandleNeedsLocator.scrollIntoViewIfNeeded();
+        await EmployeeAppAppstore(page, clinicAdminHandleNeedsLocator);
     });
 
     test("View Demo 3", async ({ page }) => {
-        const adminpanelLocator = page.locator("//div[@class='elementor-element elementor-element-336a468 elementor-align-center elementor-widget elementor-widget-elementskit-button']//div[@class='ekit-btn-wraper']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await EmployeeAppPlaystore(page, adminpanelLocator);
+        const clinicAdminViewDemoLocator3 = page.locator("//div[@class='elementor-element elementor-element-336a468 elementor-align-center elementor-widget elementor-widget-elementskit-button']//div[@class='ekit-btn-wraper']");
+        await clinicAdminViewDemoLocator3.scrollIntoViewIfNeeded();
+        await EmployeeAppPlaystore(page, clinicAdminViewDemoLocator3);
     });
 
     test("View Demo 4", async ({ page }) => {
-        const adminpanelLocator = page.locator("//div[contains(@class,'elementor-element elementor-element-66206cc elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await EmployeeAppPlaystore(page, adminpanelLocator);
+        const clinicAdminViewDemoLocator4 = page.locator("//div[contains(@class,'elementor-element elementor-element-66206cc elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
+        await clinicAdminViewDemoLocator4.scrollIntoViewIfNeeded();
+        await EmployeeAppPlaystore(page, clinicAdminViewDemoLocator4);
     });
 
     test("View Demo 5", async ({ page }) => {
-        const adminpanelLocator = page.locator("//div[contains(@class,'elementor-element elementor-element-19738bf elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
-        await adminpanelLocator.scrollIntoViewIfNeeded();
-        await EmployeeAppPlaystore(page, adminpanelLocator);
+        const clinicAdminViewDemoLocator5 = page.locator("//div[contains(@class,'elementor-element elementor-element-19738bf elementor-align-center elementor-widget elementor-widget-elementskit-button')]//a[contains(@class,'whitespace--normal')][normalize-space()='View Demo']");
+        await clinicAdminViewDemoLocator5.scrollIntoViewIfNeeded();
+        await EmployeeAppPlaystore(page, clinicAdminViewDemoLocator5);
     });
 });
